2751a5bb0e297407d280972de1e04351dd0511df
ShuhbamUpadhyayDeveloper <shubhamwork48@gmail.com>
2024-01-15 18:53:22 +0530

    Update image URLs

--- components/pages/Maincrousel.jsx
+++ components/pages/Maincrousel.jsx
@@ -33,7 +33,7 @@ function Maincrousel() {
       )}
        >
      <div className='h-[400px] xl:h-[510px] 2xl:h-[570px] lg:h-[480px] md:h-[450px] bg-slate-500 bg-center bg-cover'
-     style={{backgroundImage : `url(../../images/slider1.jpg)`}}
+     style={{backgroundImage : `url(${process.env.PUBLIC_URL}/images/slider1.jpg)`}}
       >
          <div className='bg-slate-900 absolute top-[15rem] flex flex-col gap-1 p-2 rounded-lg left-[5rem] lg:left-[8rem] xl:-left-[-13rem] xl:p-4'>
             <h1 className='flex items-center text-red-700 text-sm top-[15rem] xl:text-xl xl:mb-1'>--NEW ITEMS</h1>
@@ -42,21 +42,17 @@ function Maincrousel() {
         </div>
      </div>
           <div className='h-[400px] xl:h-[510px] 2xl:h-[570px] lg:h-[480px] md:h-[450px] bg-slate-500 bg-center bg-cover'
-          style={{backgroundImage : `url(../../images/slider2.jpg)`}}
+          style={{backgroundImage : `url(${process.env.PUBLIC_URL}/images/slider2.jpg)`}}
            >
           </div>
      <div className='h-[400px] xl:h-[510px] 2xl:h-[570px] lg:h-[480px] md:h-[450px] bg-slate-500 bg-center bg-cover'
-     style={{backgroundImage : `url(../../images/slider4.jpg)`}}
+     style={{backgroundImage : `url(${process.env.PUBLIC_URL}/images/slider4.jpg)`}}
       >
      </div>
      <div className='h-[400px] xl:h-[510px] 2xl:h-[570px] lg:h-[480px] md:h-[450px] bg-slate-500 bg-center bg-cover'
-     style={{backgroundImage : `url(../../images/slider3.jpg)`}}
+     style={{backgroundImage : `url(${process.env.PUBLIC_URL}/images/slider3.jpg)`}}
       >
-        {/* <div className='bg-slate-400 absolute top-10 text-white flex flex-col gap-1 px-3 rounded-lg '>
-            <h1>--NEW ITEMS</h1>
-            <h1>SUMMER SALE</h1>
-            <a href="#">DISCOVER MORE...</a>
-        </div> */}
+  
      </div>
    
     </Carousel>
